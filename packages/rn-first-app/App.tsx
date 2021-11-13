9cda758b589ea2bb9f816bb9e1c31872b8020211
harry <shsage91@gmail.com>
2021-11-13 17:03:41 +0900

    # first react-native app

--- packages/rn-first-app/App.tsx
+++ packages/rn-first-app/App.tsx
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react"
+import { StatusBar } from "expo-status-bar"
+import { Button, StyleSheet, Text, View } from "react-native"
+
 
 export default function App() {
+  const [ isTextChange, setIsTextChange ] = useState<boolean>(false)
+  const changeText = () => setIsTextChange(prev => !prev)
+
+  let title: string = "Open up App.tsx to start working on your app!"
+  if (isTextChange) {
+    title = "The Text Changed!"
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{ title }</Text>
+      <StatusBar style="auto"/>
+      <Button
+        title="Change Text"
+        onPress={changeText}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+})

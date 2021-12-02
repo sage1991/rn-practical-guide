8e2f0b2492d97d4868be443a116c368f0932567b
harry <shsage91@gmail.com>
2021-12-02 23:45:10 +0900

    [etc] 미사용 type 제거

--- packages/meals/store/store.ts
+++ packages/meals/store/store.ts
@@ -8,5 +8,4 @@ export const store = configureStore({ reducer, devTools: true })
 
 export type RootState = ReturnType<typeof store.getState>
 export type RootDispatch = typeof store.dispatch
-export type RootAction = Parameters<typeof store.dispatch>[0]
 export type RootStore = typeof store

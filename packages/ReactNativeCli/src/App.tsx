629e592dbe482f0f0640ae1068411408fa0a5d35
harry <shsage91@gmail.com>
2022-03-27 16:25:33 +0900

    Add dependency 'react-native-image-picker' into bare react-native app

--- packages/ReactNativeCli/src/App.tsx
+++ packages/ReactNativeCli/src/App.tsx
@@ -1,18 +1,19 @@
 import React, { FC } from "react"
 import {
-  SafeAreaView,
+  Button,
   StyleSheet,
-  Text,
   View
 } from "react-native"
+import { launchCamera } from "react-native-image-picker"
 
 export const App: FC = () => {
+  const takeImage = async () => {
+    const result = await launchCamera({ mediaType: "photo", saveToPhotos: true })
+  }
   return (
-    <SafeAreaView style={styles.root}>
-      <View>
-        <Text>hi</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.root}>
+      <Button title="Take Image" onPress={takeImage} />
+    </View>
   )
 }
 
